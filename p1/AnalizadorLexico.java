3481fa70d9a79168f976d0dea9e2a8aef63a1873
sn26 <sn26@alu.ua.es>
2021-02-21 05:52:09 +0100

    Added minor changes

--- p1/AnalizadorLexico.java
+++ p1/AnalizadorLexico.java
@@ -22,13 +22,15 @@ import java.io.RandomAccessFile;
  * 
  */
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AnalizadorLexico {
 	
 	private static char EOF = (char)-1; //Para cuando lanzamos un error
 	private RandomAccessFile fichero; //Fichero que vamos a usar
 	private ArrayList<Character> buffer = new ArrayList<>(); 
-	
+	private Token actual_tok; //Token que vamos a ir devolviendo
+	Integer[] final_states = {2,3,4,5,6,8,9,10,11,13,16,17,19,21,24,25};
 	public AnalizadorLexico(RandomAccessFile ra ){
 		this.fichero = ra; 
 	}
@@ -41,9 +43,7 @@ public class AnalizadorLexico {
 		char currentChar;
 		try {
 			currentChar = (char)fichero.readByte();
-			if(currentChar != ' ' ) { 
-				buffer.add(currentChar); // Por si tenemos que volver a alguno anterior
-			}else buffer.clear(); //Cuando hemos terminado la palabra, borramos lo que teníamos en el buffer
+			buffer.add(currentChar); // Por si tenemos que volver a alguno anterior (Lo iremos limpiando)
 			return currentChar;
 		}catch (EOFException e) {
 			return EOF; // constante estática de la clase
@@ -52,15 +52,20 @@ public class AnalizadorLexico {
 		}
 	}
 	
-	public analizadorLex() {
+	/**
+	 * Método para devolver el token actual
+	 * @return
+	 */
+	public Token siguienteToken() {
 		Integer estado = 1; //Estado inicial
 		char c = leerCaracter();  //Leemos ;
 		Integer nuevo = 0;
 		do
 		{
 			nuevo = delta(estado,c); // función de transición del DT
-			if (nuevo == EOF) errorLexico(...);
-			if (esFinal(nuevo)){
+			if (nuevo == 17 || nuevo==26) errorLexico(nuevo);
+			
+			if (Arrays.asList(this.final_states).contains(nuevo)){ //ESTADO FINAL 
 				devolverCaracteres(nuevo);
 				return tokenAsociado(nuevo); //Devolvemos el tojen de lo anterior
 				
@@ -72,26 +77,67 @@ public class AnalizadorLexico {
 		} while (true);
 
 	}
-
+	
+	/**
+	 * Función que utilizamos cunaod tenemos un error en la lectura de la cadena (error de delta)
+	 * 
+	 */
+	private void errorLexico(int nuevo) {
+		
+		return;
+	}
+	/**
+	 * Función para sacar el estado en el que nos encontramos 
+	 * @param estado 
+	 * @param c
+	 * @return
+	 */
 	private int delta(int estado, char c ) {
-		
 		switch(estado ) {
-			case 1: if(c=='(') return 2; //PARI	 
-			case 2: if(c==')') return 3; //PARD
-			case 3: if(c==':') return 4; //DOSP
-			case 4: if(c== '=') return 5; //Asign
-			case 5: if(c==';') return 6; //PyC
-			case 6: if(c=='-') {
-				//Tendremos que leer el siguiente, para saber de qué tipo es
-				if(c=='>') return 7; //Return
-				else return 8; //opas
-			}
-			case 7: if(c == '+') {
-				
-			}
+			case 1: 
+				if(c=='(') return 2; //PARI	 
+				if(c == ')') return 3; //PARD
+				if(c==':') return 4; //DOSP
+				if(c=='=') return 5; //ASSIGN
+				if(c==';') return 6; //PYC
+				if(c=='-') return 7;  //OPAS (-) O RETURN
+				if(c=='+') return 10; //OPAS (+)
+				if(c=='*') return 11; //OPMD (*)
+				if(c=='/') return 12; //OPMD (/) o Comment
+				if(Character.isLetter(c)) return 18; //ID o Palabra reservada
+				if(Character.isDigit(c)) return 20; //NINT O NENTERO O NREAL
+				return 26; //ERROR
+			case 7: //-> O - 
+				if(c=='>') return 8; //RETURN
+				return 9; //OPAS (-)
+			case 12: //OPMD (/) O (/*)
+				if(c=='*') return 14; //Comment
+				return 13;
+			case 14: //(/*)
+				if(c=='*') return 15;  
+				if(c==(char)-1) return 17; //Error eof
+				return 14; 
+			case 15: //(/*) 
+				if(c=='*') return 15;
+				if(c=='/') return 16; //Terminamos el comentario 
+				if(c==(char)-1 ) return 17; //Error LÉXICO
+				return 14;
+			case 18: //id
+				if(Character.isLetter(c) || Character.isDigit(c)) return 18; 
+				return 19; 
+			case 20: //NINT NENTERO NREAL
+				if(Character.isDigit(c)) return 20; 
+				if(c=='.') return 22; //NENTERO NREAL
+				return 21; //NINT
+			case 22: //NENTERO NREAL
+				if(Character.isDigit(c)) return 23; //NREAL
+				return 25; //Nentero
+			case 23://NREAL
+				if(Character.isDigit(c)) return 23; 
+				return 24; 
+			default: 
+				return 26; //Error
 		}
-		
-		return 1; 
 	}
 	
 }
--- p1/Token.java
+++ p1/Token.java
@@ -1,4 +1,51 @@
+import java.util.ArrayList;
 
 public class Token {
 
+	public int fila;
+	public int columna;
+
+	public String lexema;
+
+	public int tipo;		// tipo es: ID, ENTERO, REAL ...
+
+	public static final int
+		PARI 		= 0,
+		PARD		= 1,
+		DOSP            = 2,
+		RET             = 3,
+		ASIG		= 4,
+		PYC		= 5,
+		OPAS		= 6,
+		OPMD		= 7,
+		FN		= 8,
+		ENDFN		= 9,
+		INT		= 10,
+		REAL		= 11,
+		LET		= 12,
+		IF              = 13,
+		ELSE            = 14,
+		FI              = 15,
+		PRINT           = 16,
+		BLQ		= 17,
+		FBLQ		= 18,
+		ID		= 19,
+		NUMINT		= 20,
+		NUMREAL		= 21,
+		EOF		= 22;
+
+	
+	public Token() {
+		this.columna = 0;
+		this.fila= 0; 
+		this.lexema = "";
+		this.tipo = 0;
+	}
+	/**
+	 * Función que devuelve, para cada Token, la cadena de la tercera columna de la tabla del enunciado
+	 * 
+	 */
+	public String toString(){
+	        
+	}
 }
