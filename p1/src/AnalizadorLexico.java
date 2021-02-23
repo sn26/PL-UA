e8d3106df2c10ead05c79aa2c2e488959d162f1f
Alvaro <sn26@alu.ua.es>
2021-02-23 23:40:53 +0100

    Debugged analizador léxico

--- p1/src/AnalizadorLexico.java
+++ p1/src/AnalizadorLexico.java
@@ -19,7 +19,7 @@ public class AnalizadorLexico {
 	private int rollback = 0; //Contador de los caracteres que tenemos que ir hacia atras (En el caso de que haya que hacerlo)
 	//FILA Y COL DEL FICHERO	
 	private int fila = 1; 
-	private int col =1; 
+	private int col =0; 
 	
 	public AnalizadorLexico(RandomAccessFile ra ){
 		this.fichero = ra; 
@@ -34,13 +34,19 @@ public class AnalizadorLexico {
 		try {
 			if(rollback > 0){
 				currentChar = this.buffer.get(0);
-				this.buffer.remove(0);
-				rollback --; 
+				rollback --;
+				//System.out.println("Mi char es " + currentChar + " Y mi fila es " + this.fila);  
+				//this.file_position_manager(currentChar);
+				this.col ++; 
+				//System.out.println(" Y mi nueva fila es " + this.fila);  
 				return currentChar; 
 			}
-			currentChar = (char)fichero.readByte();			
+			currentChar = (char)fichero.readByte();	
+			//System.out.println("Estamos entrando con " + currentChar ); 		
 			buffer.add(currentChar); // Por si tenemos que volver a alguno anterior (Lo iremos limpiando)
-			this.file_position_manager(currentChar); 
+			//this.file_position_manager(currentChar); 
+			this.col ++; 
+			//if(currentChar == '\n') fila--; // (Lo sumaremos más adelante)
 			return currentChar;
 		}catch (EOFException e) {
 			return EOF; // constante estatica de la clase
@@ -55,8 +61,11 @@ public class AnalizadorLexico {
 	 */
 	private void file_position_manager(char currentChar  ) {
 		if(currentChar == '\n') {
-			this.col = 1; 
+			this.col = 0; 
+			//System.out.println("Estamos por la fila " + this.fila ); 
 			this.fila ++; 
+			//System.out.println("Estamos por la fila " + this.fila );  
+			return; 
 		}else this.col++; 
 	}
 	/**
@@ -73,13 +82,16 @@ public class AnalizadorLexico {
 			if (nuevo == 17 || nuevo==26 ) {
 				estado = 1; //Reseteamos el estado, por si ha saltado el error por un salto de lnea, o por un espacio
 				errorLexico(nuevo , c); //CUando encontamos un error, salimos de la ejecucicin con System.exit(-1) 
-				if(c == (char)-1 ) return this.actual_tok; //Devolvemos el token, porque hemos llegado al final del archivo
+				if(c == (char)-1 ) return this.actual_tok; //Devolvemos el token, porque hemos llegado al final del archivo      
+				nuevo = 1;
 			}
 			if (Arrays.asList(this.final_states).contains(nuevo)){ //ESTADO FINAL 
+				//if(c == '\n' ) this.fila --; 
 				devolverCaracteres(nuevo);
 				//Seteamos las filas finales del token que vamos a devolver
-				this.actual_tok.columna = this.col; 
-				this.actual_tok.fila  = this.fila; 
+				
+				this.actual_tok.fila  = this.fila;
+				//if(c == '\n') this.fila ++;  
 				return this.actual_tok; //Devolvemos el token de lo anterior
 			}
 			else{
@@ -97,7 +109,7 @@ public class AnalizadorLexico {
 	 */
 	private String get_lexema(int actual_rollback) {
 		String final_cad = ""; 
-		for(int i =0; i<buffer.size() - actual_rollback ; i++) { 
+		for(int i =0; i<buffer.size() -actual_rollback ; i++) { 
 			final_cad = final_cad + buffer.get(i);
 		}
 		return final_cad; 
@@ -114,9 +126,15 @@ public class AnalizadorLexico {
 			//ES IMPORTANTE TENER EN CUENTA EL ROLLBACK!
 			String final_cad = this.get_lexema(actual_rollback);
 			this.actual_tok.lexema = final_cad; 
+			//System.out.println("La cad final es " + final_cad ); 
+			this.actual_tok.columna = this.col - (buffer.size() -1);  
+			//System.out.println("Estamos en la col " + this.col + "Con un buffer de " + buffer.size());
 			while(buffer.size() > actual_rollback ) buffer.remove(0);
+			//System.out.println("Lo que nos queda es " + buffer.get(0)); 
 			this.rollback = actual_rollback; 
 			this.col = col - actual_rollback; 
+			//System.out.println("Estamos en la col " + this.actual_tok.columna);
+			//System.out.println("ESTAMOS ENTRANDO CON EL LEXEMA " + final_cad + " buffer.size " + buffer.size() );
 			switch(final_cad) {
 				case "fn":
 					this.actual_tok.tipo = actual_tok.FN;
@@ -155,11 +173,13 @@ public class AnalizadorLexico {
 					this.actual_tok.tipo = actual_tok.ID; 
 					return; 
 			}
+                    
 		}
 		if(nuevo == 9 || nuevo == 13 || nuevo == 21 || nuevo == 24 || nuevo == 25) {
 			if(nuevo == 25) actual_rollback= 2; 
 			String final_cad = this.get_lexema(actual_rollback);
 			this.actual_tok.lexema = final_cad; 
+			this.actual_tok.columna = this.col - (buffer.size() -1)  ;  
 			while(buffer.size() > actual_rollback ) buffer.remove(0);
 			this.rollback = actual_rollback; 
 			this.col = col - actual_rollback; 
@@ -167,7 +187,10 @@ public class AnalizadorLexico {
 		}
 		//Cuando no es ningn estado final con rollback
 		String final_cad = this.get_lexema(0);
-		this.actual_tok.lexema = final_cad; 
+		this.actual_tok.lexema = final_cad;
+		//System.out.println("Estamos en la col " + this.col);
+		//System.out.println("ESTAMOS ENTRANDO CON EL LEXEMA " + final_cad + " buffer.size " + buffer.size() );
+		this.actual_tok.columna = this.col - (buffer.size() -1) ;  
 		this.rollback = 0; 
 		this.buffer.clear();
 		return; 
@@ -180,19 +203,28 @@ public class AnalizadorLexico {
 	 */
 	private void errorLexico(int nuevo , char last_char ) {
 		if(nuevo == 17) {
+			buffer.clear( ); 
 			System.err.println("Error lexico: fin de fichero inesperado");
 			System.exit(-1);
 		}
 		
 		
 		if(last_char == '\n' || last_char == '\t' || last_char ==' ') {
-			buffer.remove(last_char); 
+			//System.out.println("ESTAMOS ENTRANDO CON " + buffer.size() + " Y CON EL CHAR " + last_char );
+			//buffer.remove(buffer.size() - 1 ); 
+			//System.out.println("Estamos entrando con " + last_char ); 
+			if(last_char == '\n') {
+				this.fila ++; 
+				this.col = 0; 
+			}
+			buffer.clear(); 
 			return ;
 		}
 		if(last_char == (char)-1 ) {
 			this.actual_tok.tipo = this.actual_tok.EOF;
 			this.actual_tok.fila = this.fila; 
 			this.actual_tok.columna = this.col;
+            buffer.clear( ); 
 			return; 
 		}
 		
@@ -209,6 +241,7 @@ public class AnalizadorLexico {
 	 * @return
 	 */
 	private int delta(int estado, char c ) {
+    	//System.out.println("ESTAMOS ENTRANDO CON " + c  + " Y con el estado " + estado); 
 		switch(estado ) {
 			case 1: 
 				if(c=='(') {
@@ -241,6 +274,7 @@ public class AnalizadorLexico {
 					return 11; //OPMD (*)
 				}
 				if(c=='/') return 12; //OPMD (/) o Comment
+                               
 				if(Character.isLetter(c)) return 18; //ID o Palabra reservada
 				if(Character.isDigit(c)) return 20; //NINT O NENTERO O NREAL
 				return 26; //ERROR
@@ -256,17 +290,33 @@ public class AnalizadorLexico {
 				this.actual_tok.tipo = this.actual_tok.OPMD; 
 				return 13;
 			case 14: //(/*)
+				if(c == '\n') {
+					this.col = 0; 
+					this.fila++; 
+				}
 				if(c=='*') return 15;  
 				if(c==(char)-1) return 17; //Error eof
 				return 14; 
 			case 15: //(/*) 
-				if(c=='*') return 15;
-				if(c=='/') return 1; //Ignoraremos el comentario 
-				if(c==(char)-1 ) return 17; //Error LXICO
+				if(c == '\n') {
+					this.col = 0; 
+					this.fila++; 
+				}
+				if(c=='*')return 15;
+				if(c=='/'){
+					buffer.clear();
+					return 1; 	
+				}
+				if(c==(char)-1 ) return 17; //Error eof
+				
 				return 14;
 			case 18: //id
-				if(Character.isLetter(c) || Character.isDigit(c)) return 18; //Continuamos leyendo el identificador
+				if(Character.isLetter(c) || Character.isDigit(c)){
+					//System.out.println("Sigo en id con " + c );  
+					return 18; //Continuamos leyendo el identificador
+				}
 				this.actual_tok.tipo = this.actual_tok.ID; 
+				//System.out.println("Estamos saliendo con " + c ); 
 				return 19; 
 			case 20: //NINT NENTERO NREAL
 				if(Character.isDigit(c)) return 20; 
